a8a6039b783c9e0c74e6cf85833c54f7e6e96d8f
Matthew Dillard <67643900+matthewdillard@users.noreply.github.com>
2021-11-21 23:10:09 -0500

    fix second set of data

--- js/cri_chart.js
+++ js/cri_chart.js
@@ -1,28 +1,20 @@
 // create 2 data_set
 var data1 = [
-  {group: "Puerto Rico", value: 7.17},
-  {group: "Myanmar", value: 10.00},
-  {group: "Haiti", value: 13.67},
-  {group: "Philippines", value: 18.17},
-  {group: "Mozambique", value: 25.83},
-  {group: "The Bahamas", value: 27.67},
-  {group: "Bangladesh", value: 28.33},
-  {group: "Pakistan", value: 29.00},
-  {group: "Thailand", value: 29.83},
-  {group: "Nepal", value: 13.33}
+  {group: "A", value: 4},
+  {group: "B", value: 16},
+  {group: "B", value: 16},
+  {group: "B", value: 16},
+  {group: "B", value: 16},
+  {group: "C", value: 8}
 ];
 
 var data2 = [
-  {group: "Puerto Rico", value: 4.12},
-  {group: "Myanmar", value: 14.35},
-  {group: "Haiti", value: 2.78},
-  {group: "Philippines", value: 0.93},
-  {group: "Mozambique", value: 0.52},
-  {group: "The Bahamas", value: 1.56},
-  {group: "Bangladesh", value: 0.38},
-  {group: "Pakistan", value: 0.30},
-  {group: "Thailand", value: 0.21},
-  {group: "Nepal", value: 0.82}
+  {group: "A", value: 7},
+  {group: "B", value: 1},
+  {group: "B", value: 16},
+  {group: "B", value: 16},
+  {group: "B", value: 16},
+  {group: "C", value: 20}
 ];
 
 // set the dimensions and margins of the graph
@@ -39,51 +31,40 @@ var svg = d3.select("#my_dataviz")
   .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialize the X axis
+// X axis
 var x = d3.scaleBand()
   .range([ 0, width ])
+  .domain(data1.map(function(d) { return d.group; }))
   .padding(0.2);
-var xAxis = svg.append("g")
+svg.append("g")
   .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
 
-// Initialize the Y axis
+// Add Y axis
 var y = d3.scaleLinear()
+  .domain([0, 20])
   .range([ height, 0]);
-var yAxis = svg.append("g")
+svg.append("g")
   .attr("class", "myYaxis")
-
+  .call(d3.axisLeft(y));
 
 // A function that create / update the plot for a given variable:
 function update(data) {
 
-  // Update the X axis
-  x.domain(data.map(function(d) { return d.group; }))
-  xAxis.call(d3.axisBottom(x))
-
-  // Update the Y axis
-  y.domain([0, d3.max(data, function(d) { return d.value }) ]);
-  yAxis.transition().duration(1000).call(d3.axisLeft(y));
-
-  // Create the u variable
   var u = svg.selectAll("rect")
     .data(data)
 
   u
     .enter()
-    .append("rect") // Add a new rect for each new elements
-    .merge(u) // get the already existing elements as well
-    .transition() // and apply changes to all of them
+    .append("rect")
+    .merge(u)
+    .transition()
     .duration(1000)
     .attr("x", function(d) { return x(d.group); })
     .attr("y", function(d) { return y(d.value); })
     .attr("width", x.bandwidth())
     .attr("height", function(d) { return height - y(d.value); })
     .attr("fill", "#69b3a2")
-
-  // If less group in the new dataset, I delete the ones not in use anymore
-  u
-    .exit()
-    .remove()
 }
 
 // Initialize the plot with the first dataset
