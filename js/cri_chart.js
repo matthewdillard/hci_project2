19879937a88f7a26290e20e45c85987b35e497e6
Matthew Dillard <67643900+matthewdillard@users.noreply.github.com>
2021-11-21 23:15:10 -0500

    fix second set of data

--- js/cri_chart.js
+++ js/cri_chart.js
@@ -1,5 +1,5 @@
 // create 2 data_set
-var data1 = [
+var cri_ = [
   {group: "Puerto Rico", value: 7.17},
   {group: "Myanmar", value: 10.00},
   {group: "Haiti", value: 13.67},
@@ -27,8 +27,8 @@ var data2 = [
 
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+  width = 700 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
