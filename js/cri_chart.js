05ad24a8a23bf7c3e3f97e2ee778256412508639
Matthew Dillard <67643900+matthewdillard@users.noreply.github.com>
2021-11-29 02:12:12 -0500

    add CRI text

--- js/cri_chart.js
+++ js/cri_chart.js
@@ -1,45 +1,47 @@
 var cri_data = [
   {group: "Puerto Rico", value: 7.17, color: "#db545a"},
   {group: "Myanmar", value: 10.00, color: "yellow"},
+  {group: "Nepal", value: 13.33, color: "darkred"},
   {group: "Haiti", value: 13.67, color: "blue"},
   {group: "Philippines", value: 18.17, color: "darkblue"},
   {group: "Mozambique", value: 25.83, color: "teal"},
   {group: "The Bahamas", value: 27.67, color: "lightblue"},
   {group: "Bangladesh", value: 28.33, color: "green"},
   {group: "Pakistan", value: 29.00, color: "darkgreen"},
-  {group: "Thailand", value: 29.83, color: "#d44f14"},
-  {group: "Nepal", value: 13.33, color: "darkred"}
+  {group: "Thailand", value: 29.83, color: "#d44f14"}
 ];
 
 var fatal_data = [
   {group: "Puerto Rico", value: 149.85, color: "#db545a"},
   {group: "Myanmar", value: 7056.45, color: "yellow"},
+  {group: "Nepal", value: 217.15, color: "darkred"},
   {group: "Haiti", value: 274.05, color: "blue"},
   {group: "Philippines", value: 859.35, color: "darkblue"},
   {group: "Mozambique", value: 125.4, color: "teal"},
   {group: "The Bahamas", value: 5.35, color: "lightblue"},
   {group: "Bangladesh", value: 572.5, color: "green"},
   {group: "Pakistan", value: 502.45, color: "darkgreen"},
-  {group: "Thailand", value: 137.75, color: "#d44f14"},
-  {group: "Nepal", value: 217.15, color: "darkred"}
+  {group: "Thailand", value: 137.75, color: "#d44f14"}
+
 ]
 
 var fatal_100_data = [
   {group: "Puerto Rico", value: 4.12, color: "#db545a"},
   {group: "Myanmar", value: 14.35, color: "yellow"},
+  {group: "Nepal", value: 0.82, color: "darkred"},
   {group: "Haiti", value: 2.78, color: "blue"},
   {group: "Philippines", value: 0.93, color: "darkblue"},
   {group: "Mozambique", value: 0.52, color: "teal"},
   {group: "The Bahamas", value: 1.56, color: "lightblue"},
   {group: "Bangladesh", value: 0.38, color: "green"},
   {group: "Pakistan", value: 0.30, color: "darkgreen"},
-  {group: "Thailand", value: 0.21, color: "#d44f14"},
-  {group: "Nepal", value: 0.82, color: "darkred"}
+  {group: "Thailand", value: 0.21, color: "#d44f14"}
 ];
 
 var losses_1M = [
   {group: "Puerto Rico", value: 4149.98, color: "#db545a"},
   {group: "Myanmar", value: 1512.11, color: "yellow"},
+  {group: "Nepal", value: 233.06, color: "darkred"},
   {group: "Haiti", value: 392.54, color: "blue"},
   {group: "Philippines", value: 3179.12, color: "darkblue"},
   {group: "Mozambique", value: 303.03, color: "teal"},
@@ -47,12 +49,12 @@ var losses_1M = [
   {group: "Bangladesh", value: 1860.04, color: "green"},
   {group: "Pakistan", value: 3771.91, color: "darkgreen"},
   {group: "Thailand", value: 7719.15, color: "#d44f14"},
-  {group: "Nepal", value: 233.06, color: "darkred"}
 ];
 
 var losses_GDP = [
   {group: "Puerto Rico", value: 3.66, color: "#db545a"},
   {group: "Myanmar", value: 0.80, color: "yellow"},
+  {group: "Nepal", value: 0.39, color: "darkred"},
   {group: "Haiti", value: 2.30, color: "blue"},
   {group: "Philippines", value: 0.54, color: "darkblue"},
   {group: "Mozambique", value: 1.33, color: "teal"},
@@ -60,20 +62,19 @@ var losses_GDP = [
   {group: "Bangladesh", value: 0.41, color: "green"},
   {group: "Pakistan", value: 0.52, color: "darkgreen"},
   {group: "Thailand", value: 0.82, color: "#d44f14"},
-  {group: "Nepal", value: 0.39, color: "darkred"}
 ];
 
 var num_events = [
   {group: "Puerto Rico", value: 24, color: "#db545a"},
   {group: "Myanmar", value: 57, color: "yellow"},
+  {group: "Nepal", value: 191, color: "darkred"},
   {group: "Haiti", value: 80, color: "blue"},
   {group: "Philippines", value: 317, color: "darkblue"},
   {group: "Mozambique", value: 57, color: "teal"},
   {group: "The Bahamas", value: 13, color: "lightblue"},
   {group: "Bangladesh", value: 185, color: "green"},
   {group: "Pakistan", value: 173, color: "darkgreen"},
-  {group: "Thailand", value: 146, color: "#d44f14"},
-  {group: "Nepal", value: 191, color: "darkred"}
+  {group: "Thailand", value: 146, color: "#d44f14"}
 ];
 
 // set the dimensions and margins of the graph
